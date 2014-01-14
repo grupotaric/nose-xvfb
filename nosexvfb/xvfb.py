9b3d48473fcd1288082d7b1c4c76173ddb2432f3
Alex Wood <awood@redhat.com>
2014-01-14 15:13:26 -0500

    Add --with-xvfb-options to accept options for XvfbWrapper

--- nosexvfb/xvfb.py
+++ nosexvfb/xvfb.py
@@ -9,9 +9,30 @@ logger = logging.getLogger('nose.plugins.xvfb')
 
 
 class Xvfb(Plugin):
+    def options(self, parser, env):
+        super(Xvfb, self).options(parser, env)
+        parser.add_option("--with-xvfb-options", action="store",
+            dest="xvfb_options",
+            default=env.get("NOSE_WITH_XVFB_OPTIONS"),
+            help="Options to pass to Xvfb. Comma delimited with "
+            "equals as separators if necessary. "
+            "E.g. \"extension=SELINUX, once\". Currently, there is no "
+            "way to provide options that begin with a +. This is a limitation "
+            "in xvfbwrapper. Repetition is not allowed. [NOSE_WITH_XVFB_OPTIONS]")
+
+    def configure(self, options, noseconfig):
+        super(Xvfb, self).configure(options, noseconfig)
+        self.xvfb_options = {}
+        opts = [x.strip() for x in options.xvfb_options.split(",")]
+        for item in opts:
+            key, sign, value = item.partition("=")
+            if not value:
+                value = ''
+            self.xvfb_options[key] = value
+
     def begin(self):
-        logger.info('Starting xvfb virtual display 1024x768')
-        self.vdisplay = XvfbWrapper(width=1024, height=768)
+        logger.info('Starting xvfb virtual display 1024x768 with %s' % self.xvfb_options)
+        self.vdisplay = XvfbWrapper(width=1024, height=768, **self.xvfb_options)
         self.vdisplay.start()
 
     def finalize(self, result):
